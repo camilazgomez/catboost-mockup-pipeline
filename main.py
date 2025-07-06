134cc2e8e194352d256ea3204a7e7298c49b6421
camilazgomez <cazumaeta1@miuandes.cl>
2025-07-06 18:50:34 -0400

    feat: update model usage to access  saved preprocessor

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Predice prob. de fracaso en pacientes nuevos.")
     parser.add_argument("--input", required=True, help="Archivo JSON con pacientes crudos")
     parser.add_argument("--output", default="predicciones.csv", help="Archivo CSV de salida")
-    parser.add_argument("--model", default="models/catboost_model.cbm", help="Ruta al modelo CatBoost entrenado")
+    parser.add_argument("--bundle", default="models/catboost_pipeline.pkl", help="Ruta al modelo + preprocesador")
 
     args = parser.parse_args()
-    pred = Predictor(model_path=args.model)
+    pred = Predictor(bundle_path=args.bundle)
     df_raw = cargar_json(args.input)
     probas = pred.predict_proba(df_raw)
 
     df_out = pd.DataFrame({"_id": df_raw["_id"], "prob_fracaso": probas})
+    df_out["pred_clase"] = (df_out["prob_fracaso"] >= 0.5).astype(int)
     df_out.to_csv(args.output, index=False)
-    print(f" Predicciones guardadas en {args.output}")
+    print(f"Predicciones guardadas en {args.output}")
--- pipeline/predictor.py
+++ pipeline/predictor.py
@@ -1,20 +1,22 @@
+import sys
 import pandas as pd
+import joblib
 from catboost import CatBoostClassifier, Pool
-from pipeline.preprocessor import prepare_features, TabularPreprocessor, ALL_COLS, CAT_COLS, BIN_COLS
+from pipeline.preprocessor import TabularPreprocessor
+from pipeline.preprocessor import prepare_features, ALL_COLS, CAT_COLS, BIN_COLS
 
 class Predictor:
-    def __init__(self, model_path):
-        self.model = CatBoostClassifier()
-        self.model.load_model(model_path)
-        self.preproc = TabularPreprocessor()
+    def __init__(self, bundle_path):
+        sys.modules["__main__"].TabularPreprocessor = TabularPreprocessor
+        bundle = joblib.load(bundle_path)
+        self.model = bundle["model"]
+        self.preproc = bundle["pp"]
 
     def predict_proba(self, df_raw: pd.DataFrame) -> list:
-        df_feat = prepare_features(df_raw)   
-        self.preproc.fit(df_feat)
+        df_feat = prepare_features(df_raw)
         X = self.preproc.transform(df_feat)
         cat_features = [X.columns.get_loc(c) for c in CAT_COLS + BIN_COLS]
         pool = Pool(X, cat_features=cat_features)
-
         return self.model.predict_proba(pool)[:, 1]
 
     
